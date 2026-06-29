7f6a8ed552d853fcf1161427af5c353c20d98d1e
Rehannah <rehannah12345@gmail.com>
2026-06-29 11:48:02 -0400

    api updates

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static("public"));
 
 app.get("/health", (req, res) => {
   res.json({
